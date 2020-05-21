3972173b82246975534c6f7ab6974ade945e01da
theBashShell <edohbruno@gmail.com>
2020-05-21 23:02:49 +0000

    more ts experiments

--- try.ts
+++ try.ts
@@ -1,1 +1,15 @@
 console.log("Deno here")
+
+enum Stages {
+    AMATEAUR = 101,
+    PRO,
+    LEGEND
+}
+
+const names : Array<[string, number]> = [
+    ["bruno", 1],
+    ["edoh", 13],
+    ["anon", 20]
+]
+
+names.forEach(el => console.log(el[0]))
